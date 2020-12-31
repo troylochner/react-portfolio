310a8e747412b865cad42183ee80f68fca243d53
troylochner <troy.lochner@gmail.com>
2020-12-31 14:17:58 -0500

    updating very basic materialize options.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,10 +3,34 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="footer">
-      <span>Employee Directory - Made with Love by Troy</span>
-      <h6>[FROM FOOTER]</h6>
-    </footer>
+
+
+    <footer className="page-footer">
+    <div className="container">
+      <div className="row">
+        <div className="col l6 s12">
+          <h5 className="white-text">Footer Content</h5>
+          <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+        </div>
+        <div className="col l4 offset-l2 s12">
+          <h5 className="white-text">Links</h5>
+          <ul>
+            <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
+            <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
+            <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
+            <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div className="footer-copyright">
+      <div className="container">
+      © 2021 Made by Troy Lochner
+      <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+      </div>
+    </div>
+  </footer>
+          
   );
 }
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,8 +5,8 @@ function Header () {
 
         <nav>
         <div className="nav-wrapper">
-          <a href="#" class="brand-logo">Logo</a>
-          <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <a href="#" className="brand-logo">Logo</a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li><a href="sass.html">Sass</a></li>
             <li><a href="badges.html">Components</a></li>
             <li><a href="collapsible.html">JavaScript</a></li>
