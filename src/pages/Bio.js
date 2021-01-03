534a11dbeb4a5eb359212db509061803840a2321
troylochner <troy.lochner@gmail.com>
2021-01-03 16:11:05 -0500

    updated bio page ; so I am more of a real person

--- src/pages/Bio.js
+++ src/pages/Bio.js
@@ -2,13 +2,43 @@ import React from "react";
 
 function Bio() {
   return (
+    <div className="container">
+    
     <div className="PersonalDetail">
-        <h1>Troy Lochner Bio</h1>
-        <p>This is just a bio page about myself. I'm not that exciting at the moment....so here is some ipsum for you.
-        </p>
-        <p>Aenean rhoncus posuere mauris. Suspendisse nisl nisi, venenatis in semper vitae, placerat id sapien. Suspendisse nunc augue, efficitur ut iaculis id, sollicitudin nec odio. Phasellus quis libero erat. Quisque luctus, dolor ac pretium lobortis, elit diam volutpat sapien, ac molestie ligula neque eget mi. Cras iaculis ex sit amet lectus ultricies, ut congue leo dapibus. Quisque sollicitudin metus nec justo eleifend, sit amet condimentum augue dignissim. Maecenas at tortor imperdiet, posuere nisi ac, blandit arcu. Aliquam hendrerit sem placerat, congue leo ac, rhoncus sem. Morbi non vulputate tellus, quis mattis nisl. Nullam vehicula malesuada justo a viverra. In magna mi, scelerisque at pretium non, viverra a est. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla eget egestas augue, blandit dapibus risus. Praesent ac turpis id lorem elementum semper.
 
-</p>
+    <div className="row">
+
+    
+      <div className="col s6 info">
+        <h3>Experience</h3>   
+        <hr></hr>
+        <p>
+
+          I am the Director of Content Workflow Systems for WWE (World Wresting Entertainment, Inc ); an integrated media organization and leader in global entertainment. In this position I am responsible for the development and deployment of software systems & processes vital to WWE's content production pipeline. I began my career at WWE as a content technician in February 2014 at the outset of the launch of WWE's award-winning OTT streaming service 'WWE Network'. In this early role at WWE I developed a database system which became critical to the operational workflow of distributing content to our network.
+          In my current role at WWE I have my hands involved in the creation and oversight of a wide range of systems; everything from a metadata mastering to creative request management, capital-expense tracking, production music licensing and media logging.
+          <p></p>
+          I came to WWE with prior experience developing databases and data-workflows in the feature-film industry where my film credits include 'The Amazing Spider-Man 2', 'Cowboys & Aliens', & 'Jack the Giant Slayer'. With earlier experience the production offices for the films 'A Serious Man', 'Paul', & 'Rampart'. All this experience gave me valuable insight into the both the creative and logistical challenges in producing content of any sort and serves as my background in how to develop or find software to suit these needs.  
+
+       
+        </p>
+      </div>
+
+      <div className="col s6">
+         <h3>Personal</h3>   
+         <hr></hr>
+         <p>
+           I currently reside in Norwalk, Connecticut but am originally from Minnesota & Wisconsin. I am a graduate of St. John's University in  Collegeville, Minnesota where I double-majored in Sociology & Communications...with a very large amount of my time spent in the art department building learning video production skills which ultimately led me in the film & content production.
+           Outside of work I am an avid tennis player; and since Covid-19, spending a lot of time exercising my artistic muscles.
+
+         </p>
+
+         <a className="downloadLink" href="../img/Resume-Troy-Lochner_jan21_dft.pdf" download>Download Resume</a>
+      </div>
+
+    </div>
+
+    </div>
+
     </div>
   );
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,14 +3,10 @@ import React from "react";
 function Contact() {
   return (
 <div className="PersonalDetail">
-
 <h1>Contact Me </h1>
-
+<hr></hr>
 <p>So you want to get in contact. Below are the easiest ways to get in touch...</p>
-
 </div>
-
-
   )
 }
 
